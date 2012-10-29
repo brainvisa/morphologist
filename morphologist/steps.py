1afaa407caf2c878e2805b8369d1cf5fb5602d36
Soizic LAGUITTON <sl231636@is147135.intra.cea.fr>
2012-10-29 15:39:07 +0100

    ENH: Bias correction and Brain segmentation
    Fixes bias correction, adds brain segmentation + test

--- morphologist/steps.py
+++ morphologist/steps.py
@@ -20,6 +20,7 @@ class BiasCorrection(Step):
 
     commissure_coordinates = None
 
+    #outputs
     hfiltered = None
 
     white_ridges = None
@@ -28,7 +29,6 @@ class BiasCorrection(Step):
 
     variance = None
 
-    # output
     mri_corrected = None
 
     def __init__(self):
@@ -42,10 +42,20 @@ class BiasCorrection(Step):
                    '-ename', self.edges, 
                    '-vname', self.variance, 
                    '-hname', self.hfiltered, 
-                   '-Points', self.commissure_coordinates]
+                   '-Points', self.commissure_coordinates,
+                   '-Kregul', "20.0",
+                   '-sampling', "16.0",
+                   '-Kcrest', "20.0",
+                   '-Grid', "2",
+                   '-ZregulTuning', "0.5",
+                   '-vp', "75",
+                   '-e', "3",
+                   '-Last', "auto"]                  
+
+        # TODO referentials
         return command
    
-
+ 
 class HistogramAnalysis(Step):
 
     mri_corrected = None
@@ -70,6 +80,45 @@ class HistogramAnalysis(Step):
                    '-mode', 'i']
         return command
 
-class ComputeBrainMask(Step):
+class BrainSegmentation(Step):
 
-  pass
+    mri_corrected = None
+
+    commissure_coordinates = None
+
+    white_ridges = None
+
+    edges = None
+
+    variance = None
+
+    histo_analysis = None
+
+    erosion_size = None
+
+    # output
+    brain_mask = None
+
+    def __init__(self):
+        super(BrainSegmentation, self).__init__()
+        self.erosion_size = 1.8
+
+    def get_command(self):
+        command = ['VipGetBrain',
+                   '-berosion', str(self.erosion_size),
+                   '-i', self.mri_corrected,
+                   '-analyse', 'r', 
+                   '-hname',  self.histo_analysis,
+                   '-bname', self.brain_mask,
+                   '-First', "0",
+                   '-Last', "0", 
+                   '-layer', "0",
+                   '-Points', self.commissure_coordinates,
+                   '-m', "V",
+                   '-Variancename', self.variance,
+                   '-Edgesname', self.edges,
+                   '-Ridge', self.white_ridges,
+                   ]
+        # TODO referentials
+        return command
+
--- tests/test_steps.py
+++ tests/test_steps.py
@@ -1,33 +1,69 @@
 import os
+import unittest
 
-from steps import BiasCorrection, HistogramAnalysis
 
-def test_bias_correction():
-    bias_correction = BiasCorrection()
+from steps import BiasCorrection, HistogramAnalysis, BrainSegmentation
 
-    base_directory = "/volatile/laguitton/data/icbm/icbm/icbm100T/t1mri/default_acquisition"
 
-    bias_correction.mri = os.path.join(base_directory, "icbm100T.ima") 
-    bias_correction.commissure_coordinates = os.path.join(base_directory, "icbm100T.APC") 
-    bias_correction.hfiltered = os.path.join(base_directory, "default_analysis/hfiltered_icbm100T.ima")
-    bias_correction.white_ridges = os.path.join(base_directory, "default_analysis/whiteridge_icbm100T.ima")
-    bias_correction.edges = os.path.join(base_directory, "default_analysis/edges_icbm100T.ima")
-    bias_correction.mri_corrected = os.path.join(base_directory, "default_analysis/nobias_icbm100T.ima")
-    bias_correction.variance = os.path.join(base_directory, "default_analysis/variance_icbm100T.ima")
-    bias_correction.run()
+class TestSteps(unittest.TestCase):
 
-def test_histogram_analysis():
-    histo_analysis = HistogramAnalysis()
+    def setUp(self):
+        base_directory = "/volatile/laguitton/data/icbm/icbm/icbm100T/t1mri/default_acquisition"
+
+        self.mri = os.path.join(base_directory, "icbm100T.ima")
+        self.commissure_coordinates = os.path.join(base_directory, "icbm100T.APC") 
+        self.hfiltered = os.path.join(base_directory, "default_analysis/hfiltered_icbm100T.ima")
+        self.white_ridges = os.path.join(base_directory, "default_analysis/whiteridge_icbm100T.ima")
+        self.edges = os.path.join(base_directory, "default_analysis/edges_icbm100T.ima")
+        self.mri_corrected = os.path.join(base_directory, "default_analysis/nobias_icbm100T.ima")
+        self.variance = os.path.join(base_directory, "default_analysis/variance_icbm100T.ima")
+        self.histo_analysis = os.path.join(base_directory, "default_analysis/nobias_icbm100T.han")
+        self.brain_mask = os.path.join(base_directory, "default_analysis/segmentation/brain_icbm100T.ima")
+
+
+    def test_bias_correction(self):
+        bias_correction = BiasCorrection()
+
+        bias_correction.mri = self.mri 
+        bias_correction.commissure_coordinates = self.commissure_coordinates 
+
+        bias_correction.hfiltered = self.hfiltered 
+        bias_correction.white_ridges = self.white_ridges
+        bias_correction.edges = self.edges 
+        bias_correction.variance = self.variance 
+        bias_correction.mri_corrected = self.mri_corrected 
+
+        bias_correction.run()
     
-    histo_analysis.mri_corrected = "/volatile/laguitton/data/icbm/icbm/icbm100T/t1mri/default_acquisition/default_analysis/nobias_icbm100T.ima"   
-    histo_analysis.histo_analysis = "/volatile/laguitton/data/icbm/icbm/icbm100T/t1mri/default_acquisition/default_analysis/nobias_icbm100T.han"
-    histo_analysis.hfiltered = "/volatile/laguitton/data/icbm/icbm/icbm100T/t1mri/default_acquisition/default_analysis/hfiltered_icbm100T.ima"
-    histo_analysis.white_ridges = "/volatile/laguitton/data/icbm/icbm/icbm100T/t1mri/default_acquisition/default_analysis/whiteridge_icbm100T.ima"
+    def test_histogram_analysis(self):
+        histo_analysis = HistogramAnalysis()
+        
+        histo_analysis.mri_corrected = self.mri_corrected 
+        histo_analysis.white_ridges = self.white_ridges 
+        histo_analysis.hfiltered = self.hfiltered 
 
-    histo_analysis.run()
+        histo_analysis.histo_analysis = self.histo_analysis
+    
+        histo_analysis.run()
+    
+    
+    def test_brain_segmentation(self):
+        brain_segmentation = BrainSegmentation()
+    
+        brain_segmentation.mri_corrected = self.mri_corrected
+        brain_segmentation.commissure_coordinates = self.commissure_coordinates
+        brain_segmentation.white_ridges = self.white_ridges
+        brain_segmentation.edges = self.edges 
+        brain_segmentation.variance = self.variance
+        brain_segmentation.histo_analysis = self.histo_analysis
+
+        brain_segmentation.brain_mask = self.brain_mask
+    
+        brain_segmentation.run()
+
 
 
 if __name__ == '__main__':
 
-   test_histogram_analysis() 
-   test_bias_correction() 
+    unittest.main()
+
