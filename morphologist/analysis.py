ebf9ebaef1bb0438a1dd022da852750dfa1973be
Soizic LAGUITTON <sl231636@is147135.intra.cea.fr>
2012-10-30 17:11:02 +0100

    ENH: adds StepFlow
    StepFlow defines the steps, parameters and parameter link of an analysis.

--- morphologist/analysis.py
+++ morphologist/analysis.py
@@ -3,19 +3,9 @@ from morphologist.steps import MockStep
 
 class Analysis(object):
      
-    def __init__(self):
-        self._steps = []
+    def __init__(self, step_flow):
+        self._step_flow = step_flow
         self._is_running = False
-
-    def propagate_paramters(self):
-        raise Exception("Analysis is an abstract class.")
-
-    def get_command_list(self):
-        self.propagate_parameters()
-        command_list = []
-        for step in self._steps:
-             command_list.append(step.get_command())
-        return command_list
 
     def run(self):
         self._is_running = True
@@ -26,8 +16,44 @@ class Analysis(object):
     def is_running(self):
         return self._is_running
 
+
+class Arguments(object):
+    
+    def __init__(self, argument_names):
+        self._argument_names = argument_names
+        for name in argument_names:
+            setattr(self, name, None)
+
+    def list_arguments(self):
+        return self._argument_names
+
+
+class OutputArguments(Arguments):
+    pass
+
+class InputArguments(Arguments):
+    pass
+
+
+class StepFlow(object):
+
+    def __init__(self):
+        self._steps = []
       
-class MockAnalysis(Analysis):
+        self.input_args = InputArgs(argument_names=[])
+        self.output_args = OutputArgs(argument_names=[])
+
+    def get_command_list(self):
+        self.propagate_parameters()
+        command_list = []
+        for step in self._steps:
+             command_list.append(step.get_command())
+        return command_list
+
+    def propagate_parameters(self):
+        raise Exception("StepFlow is an Abstract class. propagate_parameter must be redifined.") 
+      
+class MockStepFlow(StepFlow):
 
     def __init__(self):
         step1 = MockStep()
@@ -35,51 +61,38 @@ class MockAnalysis(Analysis):
         step3 = MockStep()
         self._steps = [step1, step2, step3] 
 
-        self.input_1 = None
-        self.input_2 = None
-        self.input_3 = None
-        self.input_4 = None
-        self.input_5 = None
-        self.input_6 = None
-        
-        self.output_1 = None
-        self.output_2 = None
-        self.output_3 = None
-        self.output_4 = None
-        self.output_5 = None
-        self.output_6 = None
+        self.input_args = InputArguments(argument_names=['input_1',
+                                                         'input_2',
+                                                         'input_3',
+                                                         'input_4',
+                                                         'input_5',
+                                                         'input_6'])
+        self.output_args = OutputArguments(argument_names=['output_1',
+                                                           'output_2',
+                                                           'output_3',
+                                                           'output_4',
+                                                           'output_5',
+                                                           'output_6'])
  
     def propagate_parameters(self):
 
-        self._steps[1].input_1 = self.input_1
-        self._steps[1].input_2 = self.input_2
-        self._steps[1].input_3 = self.input_3
-        self._steps[1].output_1 = self.output_1
-        self._steps[1].output_2 = self.output_2
+        self._steps[1].input_1 = self.input_args.input_1
+        self._steps[1].input_2 = self.input_args.input_2
+        self._steps[1].input_3 = self.input_args.input_3
+        self._steps[1].output_1 = self.output_args.output_1
+        self._steps[1].output_2 = self.output_args.output_2
 
         self._steps[2].input_1 = self._steps[1].output_1
         self._steps[2].input_2 = self._steps[1].output_2
-        self._steps[2].input_3 = self.input_4
-        self._steps[2].output_1 = self.output_3
-        self._steps[2].output_2 = self.output_4
+        self._steps[2].input_3 = self.input_args.input_4
+        self._steps[2].output_1 = self.output_args.output_3
+        self._steps[2].output_2 = self.output_args.output_4
 
-        self._steps[3].input_1 = self.input_5
-        self._steps[3].input_2 = self.input_6
+        self._steps[3].input_1 = self.input_args.input_5
+        self._steps[3].input_2 = self.input_args.input_6
         self._steps[3].input_3 = self._steps[2].output_1
-        self._steps[3].output_1 = self.output_5
-        self._steps[3].output_2 = self.output_6
+        self._steps[3].output_1 = self.output_args.output_5
+        self._steps[3].output_2 = self.output_args.output_6
     
  
-    #    def __init__(self):
-#        pass
-#
-#    def run(self):
-#        pass
-#
-#    def stop(self):
-#        pass
-#
-#    def is_running(self):
-#        pass
-#    
-     
+    
--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,30 +1,36 @@
 import unittest
 
-from morphologist.analysis import MockAnalysis
-
+from morphologist.analysis import MockStepFlow, Analysis
 
 def create_analysis():
-    analysis = MockAnalysis()
+    mock_step_flow = MockStepFlow()
+    analysis = Analysis(mock_step_flow)
     return analysis
+
 
 class TestAnalysis(unittest.TestCase):
 
     def setUp(self):
         self.analysis = create_analysis()
+        #TODO set in and out parameters 
+        # self.analysis.input_args.input_1 = "blabla"
 
     def test_run_analysis(self):
         self.analysis.run()
-        self.assert_(self.analysis.is_running()) 
-
+        self.assert_(self.analysis.is_running())
+  
 
     def _test_stop_analysis(self):
         self.analysis.run()
         self.analysis.stop()
         self.assert_(not self.analysis.is_running())
 
+
     def tearDown(self):
         if self.analysis.is_running():
             self.analysis.stop()
+
+
 
 if __name__ == '__main__':
     
