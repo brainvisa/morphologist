787f5ca08c8f108f723fa1c9c8175f3cbba696b1
Matthieu Perrot <matthieu.perrot@gmail.com>
2012-11-05 18:03:37 +0100

    REFAC: rework study API + add repr methods

--- morphologist/study.py
+++ morphologist/study.py
@@ -16,22 +16,36 @@ class QualityCheck(object):
 
 class Subject(object):
     def __init__(self, imgname, subjectname, groupname=None):
-        self._imgname = imgname
-        self._subjectname = subjectname
-        self._groupname = groupname
-        self._quality_check_rate = QualityCheck.UNKNOWN
+        self.imgname = imgname
+        self.subjectname = subjectname
+        self.groupname = groupname
+        self.quality_check_rate = QualityCheck.UNKNOWN
 
+    def __repr__(self):
+        s = '\t<imgname: ' + str(self.imgname) + ',\n'
+        s += '\tsubjectname: ' + str(self.subjectname) + ',\n'
+        s += '\tgroupname: ' + str(self.groupname) + ',\n'
+        s += '\tquality_check_rate: ' + str(self.quality_check_rate) + '>\n'
+        return s
 
 class Study(object):
-    def __init__(self, name):
-        self._name = name
-        self._subjects = []
+    def __init__(self, name="undefined study", outputdir=None):
+        self.name = name
+        self.outputdir = outputdir
+        self.subjects = []
 
-    def add_subject_from_file(self, filename, groupname=None):
-        subjectname = os.path.basename(filename)
+    @classmethod
+    def define_subjectname_from_filename(self, filename):
+        return os.path.splitext(os.path.basename(filename))[0]
+
+    def add_subject_from_file(self, filename, subjectname=None, groupname=None):
+        if subjectname is None:
+            subjectname = self.define_subjectname_from_filename(subjectname)
         subject = Subject(filename, subjectname, groupname)
-        self._subjects.append(subject)
+        self.subjects.append(subject)
 
-    def add_subjects_from_directory(self, dirname, groupname=None):
-        #TODO
-        pass
+    def __repr__(self):
+        s = 'name :' + str(self.name) + '\n'
+        s += 'outputdir :' + str(self.outputdir) + '\n'
+        s += 'subjects :' + repr(self.subjects) + '\n'
+        return s
