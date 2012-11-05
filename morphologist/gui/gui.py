e8550bd3d756a7b144468df038e202465b0a9de6
Matthieu Perrot <matthieu.perrot@gmail.com>
2012-11-05 18:09:45 +0100

    ENH: test study creation (unfinished)

--- morphologist/gui/gui.py
+++ morphologist/gui/gui.py
@@ -1,5 +1,5 @@
 import os, sys
-from qt_backend import QtGui, QtCore, loadUi
+from .qt_backend import QtGui, QtCore, loadUi
 from ..study import Study
 
 
@@ -33,6 +33,11 @@ ui_directory = os.path.join(prefix, 'ui')
 
 class ManageSubjectsWindow(object):
     on_apply_cancel_button_clicked_map = {}
+    default_group = 'group 1'
+    group_column_width = 100
+    GROUPNAME_COL = 0
+    SUBJECTNAME_COL = 1
+    FILENAME_COL = 2
 
     @classmethod
     def _init_class(cls):
@@ -42,24 +47,61 @@ class ManageSubjectsWindow(object):
         map[apply_role] = cls.on_apply_button_clicked
         map[reject_role] = cls.on_cancel_button_clicked
 
-    def __init__(self, parent=None, study=None):
+    def __init__(self, study, parent=None):
+        self.study = study
         ui = os.path.join(ui_directory, 'manage_subjects_of_a_study.ui')
         self.ui = loadUi(ui)
-        # TODO : fill tablewidget model with the study content
-
-        tablewidget = self.ui.subjects_tablewidget
-        tablewidget.setColumnWidth(0, 50)
 
         apply_id = QtGui.QDialogButtonBox.Apply
         cancel_id = QtGui.QDialogButtonBox.Cancel
-
         self.ui.apply_button = self.ui.apply_cancel_buttons.button(apply_id)
         self.ui.cancel_button = self.ui.apply_cancel_buttons.button(cancel_id)
 
+        # TODO : fill tablewidget model with the study content
+
+        self._init_qt_connections()
+        self._init_ui()
+
+    def _init_qt_connections(self):
+        self.ui.studyname_lineEdit.textChanged.connect(\
+                self.on_lineEdit_changed)
+        self.ui.outputdir_lineEdit.textChanged.connect(\
+                self.on_lineEdit_changed)
+        self.ui.outputdir_button.clicked.connect(\
+                self.on_outputdir_button_clicked)
         self.ui.add_one_subject_from_a_file_button.clicked.connect(\
                 self.on_add_one_subject_from_a_file_button_clicked)
         self.ui.apply_cancel_buttons.clicked.connect(\
                 self.on_apply_cancel_button_clicked)
+
+    def _init_ui(self):
+        tablewidget = self.ui.subjects_tablewidget
+        tablewidget.setColumnWidth(0, ManageSubjectsWindow.group_column_width)
+        self.ui.apply_button.setEnabled(False)
+
+    def add_subjects(self, filenames, groupname=default_group):
+        for filename in filenames:
+            self._add_subject(filename, groupname)
+
+    @QtCore.Slot("QString &")
+    def on_lineEdit_changed(self, text):
+        outputdir = self.ui.outputdir_lineEdit.text()
+        studyname = self.ui.studyname_lineEdit.text()
+        status = (outputdir != '' and studyname != '')
+        self.ui.apply_button.setEnabled(status)
+
+    @QtCore.Slot()
+    def on_outputdir_button_clicked(self):
+        caption = 'Select study output directory'
+        outputdir = self.ui.outputdir_lineEdit.text()
+        if outputdir != '':
+            default_directory = outputdir
+        else:
+            default_directory = os.getcwd()
+        selected_directory = QtGui.QFileDialog.getExistingDirectory(self.ui,
+                                                caption, default_directory) 
+        if selected_directory != '':
+            self.ui.outputdir_lineEdit.setText(selected_directory)
 
     @QtCore.Slot()
     def on_add_one_subject_from_a_file_button_clicked(self):
@@ -72,25 +114,48 @@ class ManageSubjectsWindow(object):
         ManageSubjectsWindow.on_apply_cancel_button_clicked_map[role](self)
 
     def on_apply_button_clicked(self):
-        print "apply!" #TODO
+        outputdir = self.ui.outputdir_lineEdit.text()
+        studyname = self.ui.studyname_lineEdit.text()
+        self.study.name = studyname
+        self.study.outputdir = outputdir
+        for i in range(self.ui.subjects_tablewidget.rowCount()):
+            groupname, subjectname, filename = self._get_subject_data(i)
+            self.study.add_subject_from_file(filename, subjectname, groupname)
 
     def on_cancel_button_clicked(self):
         print "cancel" #TODO
 
-    def add_subjects(self, filenames, groupname=None):
-        for filename in filenames:
-            self.add_subject(filename, groupname)
-
-    def add_subject(self, filename, groupname=None):
-        tablewidget = self.ui.subjects_tablewidget
-        tablewidget.insertRow(0)
+    def _add_subject(self, filename, groupname):
+        subjectname = Study.define_subjectname_from_filename(filename)
+        self.ui.subjects_tablewidget.insertRow(0)
         if groupname is not None:
-            groupname_item = QtGui.QTableWidgetItem(group)
-            self.ui.subjects_tablewidget.setItem(0, 0, groupname_item)
-        subject_name = os.path.splitext(os.path.basename(filename))[0]
-        subjectname_item = QtGui.QTableWidgetItem(subject_name)
-        self.ui.subjects_tablewidget.setItem(0, 1, subjectname_item)
+            self._fill_groupname(0, groupname)
+        self._fill_subjectname(0, subjectname)
+        self._fill_filename(0, filename)
+
+    def _fill_groupname(self, subject_index, groupname):
+        groupname_item = QtGui.QTableWidgetItem(groupname)
+        self.ui.subjects_tablewidget.setItem(subject_index,
+            ManageSubjectsWindow.GROUPNAME_COL, groupname_item)
+
+    def _fill_subjectname(self, subject_index, subjectname):
+        subjectname_item = QtGui.QTableWidgetItem(subjectname)
+        self.ui.subjects_tablewidget.setItem(subject_index,
+            ManageSubjectsWindow.SUBJECTNAME_COL, subjectname_item)
+
+    def _fill_filename(self, subject_index, filename):
         filename_item = QtGui.QTableWidgetItem(filename)
-        self.ui.subjects_tablewidget.setItem(0, 2, filename_item)
+        self.ui.subjects_tablewidget.setItem(subject_index,
+            ManageSubjectsWindow.FILENAME_COL, filename_item)
+
+    def _get_subject_data(self, subject_index):
+        tablewidget = self.ui.subjects_tablewidget
+        groupname = tablewidget.item(subject_index,
+                        ManageSubjectsWindow.GROUPNAME_COL).text()
+        subjectname = tablewidget.item(subject_index,
+                        ManageSubjectsWindow.SUBJECTNAME_COL).text()
+        filename = tablewidget.item(subject_index,
+                        ManageSubjectsWindow.FILENAME_COL).text()
+        return groupname, subjectname, filename
 
 ManageSubjectsWindow._init_class()
--- tests/gui/test_study_creation.py
+++ tests/gui/test_study_creation.py
@@ -11,32 +11,46 @@ def test_gui(test):
     timer.singleShot(0, test) 
     sys.exit(qApp.exec_())
 
-prefix = '/neurospin/lnao/Panabase/cati-dev-prod/morphologist/raw_irm'
-
-def action_select_some_input_subjects(manage_subjects_window):
+def define_new_study_content(manage_subjects_window,
+                studyname, outputdir, filenames):
     ui = manage_subjects_window.ui
-    for filename in ['caca.ima', 'chaos.ima.gz',
-                     'dionysos2.ima', 'hyperion.nii']:
+
+    # set studyname and output dir
+    ui.studyname_lineEdit.setText(studyname)
+    ui.outputdir_lineEdit.setText(outputdir)
+
+    # select some subjects
+    for filename in filenames:
         QtTest.QTest.mouseClick(ui.add_one_subject_from_a_file_button,
                                 QtCore.Qt.LeftButton)
         dialog = ui.findChild(QtGui.QFileDialog, 'SelectSubjectsDialog')
-        full_name = os.path.join(prefix, filename)
-        dialog.selectFile(full_name)
+        dialog.selectFile(filename)
         dialog.accept()
         dialog.deleteLater()
         QtGui.qApp.sendPostedEvents(dialog, QtCore.QEvent.DeferredDelete)
 
+    # apply
+    QtTest.QTest.mouseClick(ui.apply_button, QtCore.Qt.LeftButton)
+
+
+prefix = '/neurospin/lnao/Panabase/cati-dev-prod/morphologist/raw_irm'
 
 def test_ManageSubjectsWindow():
-    study = Study('my_study')
+    filenames = [os.path.join(prefix, filename) for filename in \
+                ['caca.ima', 'chaos.ima.gz', 'dionysos2.ima', 'hyperion.nii']]
+    studyname = 'my_study'
+    outputdir = '/neurospin/lnao/Panabase/cati-dev-prod/morphologist/studies/my_study'
+    study = Study(studyname)
 
     global manage_subjects_window
 
-    manage_subjects_window = ManageSubjectsWindow()
+    print "** study before = \n", study
+    manage_subjects_window = ManageSubjectsWindow(study)
     manage_subjects_window.ui.show()
-    action_select_some_input_subjects(manage_subjects_window)
-    #window.ui.close()
-    #QtTest.QTest.mouseClick(window.apply_button, QtCore.Qt.LeftButton)
-    #QtGui.qApp.quit()
+    define_new_study_content(manage_subjects_window,
+                    studyname, outputdir, filenames)
+    print "** study after = \n", study
+    manage_subjects_window.ui.close()
+    QtGui.qApp.quit()
 
 test_gui(test_ManageSubjectsWindow)
