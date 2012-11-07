50613e82b836df5de571ad261dfb94db4b9e8eff
Soizic LAGUITTON <sl231636@is147135.intra.cea.fr>
2012-11-07 10:53:06 +0100

    ENH: unique id for subject: subject name

--- morphologist/study.py
+++ morphologist/study.py
@@ -4,16 +4,13 @@ import os
 
 
 class Subject(object):
-    def __init__(self, imgname, subjectname, groupname=None)
+    def __init__(self, imgname, subjectname, groupname=None):
         self.imgname = imgname
-        self.subjectname = subjectname
         self.groupname = groupname
 
     def __repr__(self):
         s = '\t<imgname: ' + str(self.imgname) + ',\n'
-        s += '\tsubjectname: ' + str(self.subjectname) + ',\n'
         s += '\tgroupname: ' + str(self.groupname) + ',\n'
-        s += '\tquality_check_rate: ' + str(self.quality_check_rate) + '>\n'
         return s
 
 
@@ -21,8 +18,8 @@ class Study(object):
     def __init__(self, name="undefined study", outputdir=None):
         self.name = name
         self.outputdir = outputdir
-        self.subjects = []
-        self._analysis = None
+        self.subjects = {}
+        self.analysis = {}
 
     @classmethod
     def define_subjectname_from_filename(self, filename):
@@ -31,11 +28,22 @@ class Study(object):
     def add_subject_from_file(self, filename, subjectname=None, groupname=None):
         if subjectname is None:
             subjectname = self.define_subjectname_from_filename(subjectname)
+        if subjectname in self.subjects:
+            raise SubjectNameExistsError("subjectname")
         subject = Subject(filename, subjectname, groupname)
-        self.subjects.append(subject)
+        self.subjects[subjectname] = subject
+
+    def list_subject_names(self):
+       return self.subjects.keys()
 
     def __repr__(self):
         s = 'name :' + str(self.name) + '\n'
         s += 'outputdir :' + str(self.outputdir) + '\n'
         s += 'subjects :' + repr(self.subjects) + '\n'
         return s
+
+
+class SubjectNameExistsError(Exception):
+    pass
+
+
