11a2dbc0d552414b3ef4f39954aa7133159a4ccb
Matthieu Perrot <matthieu.perrot@gmail.com>
2012-11-08 18:28:30 +0100

    TEST: add unfinished test for main window study widget

---
+++ morphologist/tests/gui/test_main_window.py
@@ -0,0 +1,41 @@
+import os, sys
+import types
+import unittest
+
+from morphologist.gui.qt_backend import QtGui, QtCore, QtTest
+from morphologist.gui import create_main_window
+from morphologist.tests.gui import TestGui
+from morphologist.tests.study import FlatFilesStudyTestCase
+
+class TestStudyWidget(TestGui):
+
+    def __init__(self, *args, **kwargs):
+        super(TestStudyWidget, self).__init__(*args, **kwargs)
+
+    def setUp(self):
+        self.test_case = self._create_test_case()
+
+    @TestGui.start_qt_and_test
+    def test_start_main_window(self):
+        global main_window
+        main_window = create_main_window()
+        main_window.ui.show()
+        self.test_case.create_study()
+        #main_window.study.
+        # main_window.ui.close() #FIXME: uncomment
+
+
+class TestFlatFilesStudyWidget(TestStudyWidget):
+
+    def __init__(self, *args, **kwargs):
+        super(TestFlatFilesStudyWidget, self).__init__(*args, **kwargs)
+
+    def _create_test_case(self):
+        test_case = FlatFilesStudyTestCase()
+        return test_case
+
+
+if __name__ == '__main__':
+    qApp = QtGui.QApplication(sys.argv)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestFlatFilesStudyWidget)
+    unittest.TextTestRunner(verbosity=2).run(suite)
