d9848f02980b755b40ca2e5ae12cbcf1fbc033c6
Matthieu Perrot <matthieu.perrot@gmail.com>
2012-11-09 14:34:08 +0100

    ENH: GUI tests can be started from ipython
    - add settings definitions to disable qt event loop in tests
    - add a method to directly instantiate and call unit tests from ipython

--- morphologist/tests/gui/__init__.py
+++ morphologist/tests/gui/__init__.py
@@ -2,15 +2,24 @@ import types
 import unittest
 
 from morphologist.gui.qt_backend import QtCore, QtGui
+from morphologist import settings
 
+if settings['start_qt_event_loop_for_tests']:
+    class TestGui(unittest.TestCase):
 
-class TestGui(unittest.TestCase):
+        @staticmethod
+        def start_qt_and_test(test):
+            def func(self):
+                timer = QtCore.QTimer()
+                func = types.MethodType(test, self, self.__class__)
+                timer.singleShot(0, func)
+                self.assertFalse(QtGui.qApp.exec_())
+            return func
+else:
+    class TestGui(unittest.TestCase):
 
-    @staticmethod
-    def start_qt_and_test(test):
-        def func(self):
-            timer = QtCore.QTimer()
-            func = types.MethodType(test, self, self.__class__)
-            timer.singleShot(0, func)
-            self.assertFalse(QtGui.qApp.exec_())
-        return func
+        @staticmethod
+        def start_qt_and_test(test):
+            def func(self):
+                test(self)
+            return func
