32bf0b8b848207a75bbf026b43a4bbbda274c096
Dominique Geffroy <dominique.geffroy@cea.fr>
2012-11-13 10:42:47 +0100

    TEST: Test analysis step by step ok except one test: the minf of the white ridge produced by bias correction is different but it is re-written correctly in brain mask, so it is not a big problem.

--- morphologist/tests/test_steps.py
+++ morphologist/tests/test_steps.py
@@ -12,53 +12,67 @@ from brainvisa.data import neuroHierarchy
 class TestIntraAnalysisSteps(unittest.TestCase):
     
     def create_ref_database(self):
-        if not os.path.exists(self.bv_db_directory):
-            os.makedirs( self.bv_db_directory )
-            brainvisa.axon.initializeProcesses()
-            database_settings = neuroConfig.DatabaseSettings( self.bv_db_directory )
-            database = neuroHierarchy.SQLDatabase( os.path.join(self.bv_db_directory, "database.sqlite"), 
-                                                   self.bv_db_directory, 
-                                                   'brainvisa-3.1.0', 
-                                                   context=defaultContext(), 
-                                                   settings=database_settings )
-            neuroHierarchy.databases.add( database )
-            neuroConfig.dataPath.append( database_settings )
-            input = "/neurospin/lnao/Panabase/cati-dev-prod/morphologist/raw_irm/%s.nii" % self.subject
-            t1mri = {"_database" : database.name, '_format' : 'NIFTI-1 image', 
-                         "protocol" : "test", "subject" : self.subject}
-            defaultContext().runProcess('ImportT1MRI', input, t1mri)
+        os.makedirs( self.bv_db_directory )
+        brainvisa.axon.initializeProcesses()
+        database_settings = neuroConfig.DatabaseSettings( self.bv_db_directory )
+        database = neuroHierarchy.SQLDatabase( os.path.join(self.bv_db_directory, "database.sqlite"), 
+                                               self.bv_db_directory, 
+                                               'brainvisa-3.1.0', 
+                                               context=defaultContext(), 
+                                               settings=database_settings )
+        neuroHierarchy.databases.add( database )
+        neuroConfig.dataPath.append( database_settings )
+        raw_mri = "/neurospin/lnao/Panabase/cati-dev-prod/morphologist/raw_irm/%s.nii" % self.subject
+        t1mri = {"_database" : database.name, '_format' : 'NIFTI-1 image', 
+                     "protocol" : "test", "subject" : self.subject}
+        defaultContext().runProcess('ImportT1MRI', raw_mri, t1mri)
 
-            pipeline=brainvisa.processes.getProcessInstance("morphologist")
-            nodes=pipeline.executionNode()
-            ac = [124.010253906, 110.013366699, 74.0999832153]
-            pc = [124.383506775, 136.140884399, 75.3999862671]
-            ip = [123.263755798, 102.175109863, 26.0]
-            # select steps until split brain and fix the random seed
-            nodes.child('TalairachTransformation').setSelected(0)
-            nodes.child('GreyWhiteClassification').setSelected(0)
-            nodes.child('GreyWhiteSurface').setSelected(0)
-            nodes.child('HemispheresMesh').setSelected(0)
-            nodes.child('HeadMesh').setSelected(0)
-            nodes.child('CorticalFoldsGraph').setSelected(0)
-            nodes.child('BiasCorrection').fix_random_seed = True
-            nodes.child('HistoAnalysis').fix_random_seed = True
-            nodes.child('BrainSegmentation').fix_random_seed = True
-            nodes.child('SplitBrain').fix_random_seed = True
-    
-            print "* Run Axon Morphologist to get reference results"
-            defaultContext().runProcess(pipeline, t1mri, Anterior_Commissure=ac, 
-                                  Posterior_Commissure=pc, Interhemispheric_Point=ip)
-            
-            brainvisa.axon.cleanup()
+        pipeline=brainvisa.processes.getProcessInstance("morphologist")
+        nodes=pipeline.executionNode()
+        ac = [124.010253906, 110.013366699, 74.0999832153]
+        pc = [124.383506775, 136.140884399, 75.3999862671]
+        ip = [123.263755798, 102.175109863, 26.0]
+        # select steps until split brain and fix the random seed
+        nodes.child('TalairachTransformation').setSelected(0)
+        nodes.child('GreyWhiteClassification').setSelected(0)
+        nodes.child('GreyWhiteSurface').setSelected(0)
+        nodes.child('HemispheresMesh').setSelected(0)
+        nodes.child('HeadMesh').setSelected(0)
+        nodes.child('CorticalFoldsGraph').setSelected(0)
+        nodes.child('BiasCorrection').fix_random_seed = True
+        nodes.child('HistoAnalysis').fix_random_seed = True
+        nodes.child('BrainSegmentation').fix_random_seed = True
+        nodes.child('SplitBrain').fix_random_seed = True
+
+        print "* Run Axon Morphologist to get reference results"
+        print "* First until the bias correction to save the first white ridge result"
+        nodes.child('HistoAnalysis').setSelected(0)
+        nodes.child('BrainSegmentation').setSelected(0)
+        nodes.child('SplitBrain').setSelected(0)
+        defaultContext().runProcess(pipeline, t1mri, Anterior_Commissure=ac, 
+                              Posterior_Commissure=pc, Interhemispheric_Point=ip)
+        # Save the white ridge in another file because it will be re-written
+        shutil.copy(os.path.join(self.base_directory, self.white_ridges), 
+                    os.path.join(self.base_directory, self.white_ridges_bc))
+        shutil.copy(os.path.join(self.base_directory, self.white_ridges+".minf"), 
+                    os.path.join(self.base_directory, self.white_ridges_bc+".minf"))
+        
+        print "* Then until the split brain"
+        nodes.child('PrepareSubject').setSelected(0)
+        nodes.child('BiasCorrection').setSelected(0)
+        nodes.child('HistoAnalysis').setSelected(1)
+        nodes.child('BrainSegmentation').setSelected(1)
+        nodes.child('SplitBrain').setSelected(1)
+        defaultContext().runProcess(pipeline, t1mri)
+        
+        brainvisa.axon.cleanup()
 
         
-    def setUp(self):      
+    def setUp(self):
         self.subject = "hyperion"
         self.bv_db_directory = "/neurospin/lnao/Panabase/cati-dev-prod/morphologist/bv_database"
         self.base_directory = "/neurospin/lnao/Panabase/cati-dev-prod/morphologist/bv_database/test/%s/t1mri/default_acquisition" % self.subject
         self.output_directory = "/tmp/morphologist_test_steps"
-        
-        self.create_ref_database()
         
         if os.path.exists(self.output_directory):
             shutil.rmtree(self.output_directory)
@@ -66,139 +80,162 @@ class TestIntraAnalysisSteps(unittest.TestCase):
   
         self.mri = os.path.join(self.base_directory, 
                                 "%s.nii" % self.subject)
-        self.commissure_coordinates = os.path.join(self.base_directory, 
-                                      "%s.APC" % self.subject) 
+        
+        self.commissure_coordinates = "%s.APC" % self.subject 
         
         if not os.path.exists(os.path.join(self.output_directory, "registration")):
             os.mkdir(os.path.join(self.output_directory, "registration"))
 
-        self.talairach_transform = os.path.join(self.output_directory, 
-                                   "registration/RawT1-%s_default_acquisition_TO_Talairach-MNI.trm" % self.subject)
+        self.talairach_transform = os.path.join("registration", 
+                                                "RawT1-%s_default_acquisition_TO_Talairach-MNI.trm" 
+                                                % self.subject)
         
         if not os.path.exists(os.path.join(self.output_directory, "default_analysis")):
             os.mkdir(os.path.join(self.output_directory, "default_analysis"))
-        self.hfiltered = os.path.join(self.output_directory, 
-                         "default_analysis/hfiltered_%s.nii" % self.subject)
-        self.white_ridges = os.path.join(self.output_directory, 
-                            "default_analysis/whiteridge_%s.nii" % self.subject)
-        self.edges = os.path.join(self.output_directory, 
-                     "default_analysis/edges_%s.nii" % self.subject)
-        self.mri_corrected = os.path.join(self.output_directory, 
-                             "default_analysis/nobias_%s.nii" % self.subject)
-        self.variance = os.path.join(self.output_directory, 
-                        "default_analysis/variance_%s.nii" % self.subject)
-        self.histo_analysis = os.path.join(self.output_directory, 
-                              "default_analysis/nobias_%s.han" % self.subject)
+            
+        self.hfiltered = os.path.join("default_analysis", "hfiltered_%s.nii" % self.subject)
+        self.white_ridges = os.path.join("default_analysis", "whiteridge_%s.nii" % self.subject)
+        self.white_ridges_bc = os.path.join("default_analysis", "whiteridge_%s_bc.nii" % self.subject)
+        self.edges = os.path.join("default_analysis", "edges_%s.nii" % self.subject)
+        self.mri_corrected = os.path.join("default_analysis", "nobias_%s.nii" % self.subject)
+        self.variance = os.path.join("default_analysis", "variance_%s.nii" % self.subject)
+        self.histo_analysis = os.path.join("default_analysis", "nobias_%s.han" % self.subject)
         
         if not os.path.exists(os.path.join(self.output_directory, "default_analysis", "segmentation")):
             os.mkdir(os.path.join(self.output_directory, "default_analysis", "segmentation"))
-        self.brain_mask = os.path.join(self.output_directory, 
-                          "default_analysis/segmentation/brain_%s.nii" % self.subject)
-        self.split_mask = os.path.join(self.output_directory, 
-                          "default_analysis/segmentation/voronoi_%s.nii" % self.subject)
+        self.brain_mask = os.path.join("default_analysis", "segmentation", "brain_%s.nii" % self.subject)
+        self.split_mask = os.path.join("default_analysis", "segmentation", "voronoi_%s.nii" % self.subject)
+        
+        if not os.path.exists(self.bv_db_directory):
+            self.create_ref_database()
 
-    def run_spatial_normalization(self):
+
+    def test_spatial_normalization(self):
         spatial_normalization = SpatialNormalization()
 
         spatial_normalization.mri = self.mri
 
-        spatial_normalization.commissure_coordinates = self.commissure_coordinates
-        spatial_normalization.talairach_transform = self.talairach_transform
+        spatial_normalization.commissure_coordinates = os.path.join(self.output_directory,
+                                                                    self.commissure_coordinates)
+        spatial_normalization.talairach_transform = os.path.join(self.output_directory,
+                                                                 self.talairach_transform)
 
         self.assert_(spatial_normalization.run() == 0)
 
 
-    def run_bias_correction(self):
+    def test_bias_correction(self):
         bias_correction = BiasCorrection()
 
         bias_correction.mri = self.mri 
-        bias_correction.commissure_coordinates = self.commissure_coordinates
+        bias_correction.commissure_coordinates = os.path.join(self.base_directory, 
+                                                              self.commissure_coordinates)
         bias_correction.fix_random_seed = True
         
-        bias_correction.hfiltered = self.hfiltered 
-        bias_correction.white_ridges = self.white_ridges
-        bias_correction.edges = self.edges 
-        bias_correction.variance = self.variance 
-        bias_correction.mri_corrected = self.mri_corrected 
+        bias_correction.hfiltered = os.path.join(self.output_directory, self.hfiltered) 
+        bias_correction.white_ridges = os.path.join(self.output_directory, self.white_ridges_bc)
+        bias_correction.edges = os.path.join(self.output_directory, self.edges) 
+        bias_correction.variance = os.path.join(self.output_directory, self.variance) 
+        bias_correction.mri_corrected = os.path.join(self.output_directory, self.mri_corrected) 
 
         self.assert_(bias_correction.run() == 0)
+        
+        self.compare_results([self.hfiltered, self.white_ridges_bc, self.edges, 
+                              self.variance, self.mri_corrected])
+        
             
 
-    def run_histogram_analysis(self):
+    def test_histogram_analysis(self):
         histo_analysis = HistogramAnalysis()
         
-        histo_analysis.mri_corrected = self.mri_corrected 
-        histo_analysis.white_ridges = self.white_ridges 
-        histo_analysis.hfiltered = self.hfiltered
+        histo_analysis.mri_corrected = os.path.join(self.base_directory, self.mri_corrected)
+        histo_analysis.white_ridges = os.path.join(self.base_directory, self.white_ridges_bc)
+        histo_analysis.hfiltered = os.path.join(self.base_directory, self.hfiltered)
         histo_analysis.fix_random_seed = True
 
-        histo_analysis.histo_analysis = self.histo_analysis
+        histo_analysis.histo_analysis = os.path.join(self.output_directory, self.histo_analysis)
     
         self.assert_(histo_analysis.run() == 0)
     
+        self.compare_results([self.histo_analysis, self.histo_analysis.replace(".han", ".his")])
+        
     
-    def run_brain_segmentation(self):
+    def test_brain_segmentation(self):
         brain_segmentation = BrainSegmentation()
     
-        brain_segmentation.mri_corrected = self.mri_corrected
-        brain_segmentation.commissure_coordinates = self.commissure_coordinates
-        brain_segmentation.white_ridges = self.white_ridges
-        brain_segmentation.edges = self.edges 
-        brain_segmentation.variance = self.variance
-        brain_segmentation.histo_analysis = self.histo_analysis
+        brain_segmentation.mri_corrected = os.path.join(self.base_directory, self.mri_corrected)
+        brain_segmentation.commissure_coordinates = os.path.join(self.base_directory, 
+                                                                 self.commissure_coordinates)
+        brain_segmentation.edges = os.path.join(self.base_directory, self.edges)
+        brain_segmentation.variance = os.path.join(self.base_directory, self.variance)
+        brain_segmentation.histo_analysis = os.path.join(self.base_directory, self.histo_analysis)
         brain_segmentation.fix_random_seed = True
 
-        brain_segmentation.brain_mask = self.brain_mask
-    
+        brain_segmentation.brain_mask = os.path.join(self.output_directory, self.brain_mask)
+        brain_segmentation.white_ridges = os.path.join(self.output_directory, self.white_ridges)
+
+        # copy white ridge file into the output directory because it is an input/output
+        shutil.copy(os.path.join(self.base_directory, self.white_ridges_bc), 
+                    brain_segmentation.white_ridges)
+        shutil.copy(os.path.join(self.base_directory, self.white_ridges_bc+".minf"), 
+                    brain_segmentation.white_ridges+".minf")
+
         self.assert_(brain_segmentation.run() == 0)
 
+        self.compare_results([self.brain_mask, self.white_ridges])
+        
 
-    def run_split_brain(self):
+    def test_split_brain(self):
         split_brain = SplitBrain()
 
-        split_brain.mri_corrected = self.mri_corrected
-        split_brain.brain_mask = self.brain_mask
-        split_brain.white_ridges = self.white_ridges
-        split_brain.histo_analysis = self.histo_analysis
-        split_brain.commissure_coordinates = self.commissure_coordinates
+        split_brain.mri_corrected = os.path.join(self.base_directory, self.mri_corrected)
+        split_brain.brain_mask = os.path.join(self.base_directory, self.brain_mask)
+
+        split_brain.histo_analysis = os.path.join(self.base_directory, self.histo_analysis)
+        split_brain.commissure_coordinates = os.path.join(self.base_directory, self.commissure_coordinates)
+        split_brain.white_ridges = os.path.join(self.base_directory, self.white_ridges)
         split_brain.fix_random_seed = True
 
-        split_brain.split_mask = self.split_mask
-
+        split_brain.split_mask = os.path.join(self.output_directory, self.split_mask)
+                
         self.assert_(split_brain.run() == 0)
         
+        self.compare_results([self.split_mask])
+     
         
-    def test_steps(self):
-        self.run_spatial_normalization()
-        self.run_bias_correction()
-        self.run_histogram_analysis()
-        self.run_brain_segmentation()
-        self.run_split_brain()
-        
-        for (dirpath, dirnames, filenames) in os.walk(self.output_directory):
-            for f in filenames:
-                f_test = os.path.join(dirpath, f)
-                f_ref = os.path.join(self.base_directory, os.path.relpath(dirpath, self.output_directory), f)
-                if f.endswith(".minf"):
-                    minf_test = eval(open(f_test).read()[13:])
-                    minf_ref = eval(open(f_ref).read()[13:])
-                    attributes = set(minf_ref.keys())
-                    if "uuid" in attributes:
-                        attributes.remove("uuid")
-                    if "referential" in attributes:
-                        attributes.remove("referential")
-                    for att in attributes:
-                        self.assert_(minf_test.get(att) == minf_ref[att], 
-                                     "The content of %s in test is different from the reference results\
-for the attribute %s.The reference value is %s, whereas the test value is %s."
-                                     % (f, att, str(minf_ref.get(att)), str(minf_test.get(att))))
-                else:
-                    f_test = os.path.join(dirpath, f)
-                    f_ref = os.path.join(self.base_directory, os.path.relpath(dirpath, self.output_directory), f)
-                    self.assertTrue(comp_files(f_ref, f_test), 
-                                    "The content of "+f+" in test is different from the reference results.")
+    def compare_results(self, results):
+        for f in results:
+            f_ref = os.path.join(self.base_directory, f)
+            f_test = os.path.join(self.output_directory, f)
+            self.assert_(comp_files(f_ref, f_test), 
+                         "The content of %s in test is different from the reference results." 
+                         % os.path.basename(f))
+            self.compare_minf(f_ref+".minf", f_test+".minf")
+          
+          
+    def compare_minf(self, f_minf_ref, f_minf_test):
+        # The minf files are not always written the same way 
+        # but the value of the common attributes should be the same
+        if not os.path.exists(f_minf_ref):
+            self.assertFalse(os.path.exists(f_minf_test))
+        elif os.path.exists(f_minf_test):
+            minf_test = eval(open(f_minf_test).read()[13:])
+            minf_ref = eval(open(f_minf_ref).read()[13:])
+            attributes_ref = set(minf_ref.keys())
+            if "uuid" in attributes_ref:
+                attributes_ref.remove("uuid")
+            if "referential" in attributes_ref:
+                attributes_ref.remove("referential")
+            attributes_test = set(minf_test.keys())
+            difference = attributes_ref.difference(attributes_test)
+            self.assert_(difference == set(),
+                         "The content of %s in test is different from the reference results. \
+Some attributes are not the same: %s." % (os.path.basename(f_minf_ref), str(difference)) )
+            for att in attributes_ref:
+                self.assert_(minf_test.get(att) == minf_ref[att], 
+                             "The content of %s in test is different from the reference results\
+for the attribute %s. The reference value is %s, whereas the test value is %s."
+% (os.path.basename(f_minf_ref), att, str(minf_ref.get(att)), str(minf_test.get(att)) ) )
 
-    
 
 def comp_files(nfc1, nfc2, lgbuf=32*1024):
     """Compare les 2 fichiers et renvoie True seulement s'ils ont un contenu 
