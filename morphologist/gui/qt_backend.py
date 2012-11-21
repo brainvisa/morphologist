e0e884e5474f772c5b918d1ef38a42ebf24364fd
Matthieu Perrot <matthieu.perrot@gmail.com>
2012-11-21 13:55:07 +0100

    WRG: disable pyside qt backend which does not work
    - the old code is available in the branch pyqt_pyside_switch
    - pylint and eclipse complain because the switch break introspection
      mechanisms

--- morphologist/gui/qt_backend.py
+++ morphologist/gui/qt_backend.py
@@ -1,52 +1,27 @@
 import imp, __builtin__
 
-def choose_pyqt_backend():
-    import sip
-    API_VERSION = 2
-    qt_api = ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl", "QVariant"]
-    for qt_module in qt_api:
-        sip.setapi(qt_module, API_VERSION)
-    try:
-        module_info = imp.find_module('PyQt4')
-    except ImportError:
-        raise Exception("error: missing PyQt dependency.")
-    module = imp.load_module('PyQt4', *module_info)
-    submodules = ['QtCore', 'QtGui', 'QtTest', 'Qt'] #TODO : add missing modules
-    for submodule in submodules:
-        __builtin__.__import__('PyQt4.' + submodule)
-        globals()[submodule] = module.__getattribute__(submodule)
-        
-    import PyQt4.QtCore
-    PyQt4.QtCore.Slot = PyQt4.QtCore.pyqtSlot
-    PyQt4.QtCore.Signal = PyQt4.QtCore.pyqtSignal
-    global loadUi, loadUiType
-    def loadUi(uifile, baseinstance=None):
-        from PyQt4 import uic
-        return uic.loadUi(uifile, baseinstance=baseinstance)
-    def loadUiType(uifile):
-        from PyQt4 import uic
-        return uic.loadUiType(uifile)
+import sip
+API_VERSION = 2
+qt_api = ["QDate", "QDateTime", "QString", "QTextStream", "QTime", "QUrl", "QVariant"]
+for qt_module in qt_api:
+    sip.setapi(qt_module, API_VERSION)
+
+try:
+    import PyQt4
+except ImportError:
+    raise Exception("error: missing PyQt dependency.")
+else:
+    from PyQt4 import QtCore, QtGui, QtTest, Qt
+
+import PyQt4.QtCore
+PyQt4.QtCore.Slot = PyQt4.QtCore.pyqtSlot
+PyQt4.QtCore.Signal = PyQt4.QtCore.pyqtSignal
 
 
+def loadUi(uifile, baseinstance=None):
+    from PyQt4 import uic
+    return uic.loadUi(uifile, baseinstance=baseinstance)
 
-def choose_pyside_backend():
-    try:
-        module_info = imp.find_module('PySide')
-    except ImportError:
-        raise Exception("error: missing PySide dependency.")
-    imp.load_module('morphologist.gui.qt_backend', *module_info)
-
-    global loadUi
-    def loadUi(uifile, baseinstance=None):
-        from PySide.QtUiTools import QUiLoader
-        from PySide.QtCore import QMetaObject
-        loader = QUiLoader(baseinstance)
-        ui = loader.load(uifile)
-        QMetaObject.connectSlotsByName(ui)
-        return ui
-
-
-# Choose the python bindings you want to use for QT :
-# PyQt (API 2) or PySide
-choose_pyqt_backend()
-#choose_pyside_backend()
+def loadUiType(uifile):
+    from PyQt4 import uic
+    return uic.loadUiType(uifile)
