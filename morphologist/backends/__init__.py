bd4f1c045538fdb31ca1f234fe71d5ff0825abd9
Matthieu Perrot <matthieu.perrot@gmail.com>
2012-11-27 12:36:00 +0100

    ENH: add a basic backend framework, extensible with mixin interface

---
+++ morphologist/backends/__init__.py
@@ -0,0 +1,54 @@
+from morphologist import settings
+
+
+class Backend(object):
+    DISPLAY_ROLE = 'display_backend'
+    _backend_instance = {}
+    _backend_loading_info = {
+        'pyanatomist' : ('morphologist.backends.pyanatomist_backend',
+                         'PyanatomistBackend')
+    }
+
+    @classmethod
+    def display_backend(cls):
+        return cls.backend_from_role(cls.DISPLAY_ROLE)
+
+    @classmethod
+    def select_display_backend(cls, backend_name):
+        cls.select_backend_from_role(cls.DISPLAY_ROLE)
+
+    @classmethod
+    def backend_from_role(cls, role):
+        try:
+            instance = cls._backend_instance[role]
+        except KeyError:
+            cls.select_backend_from_role(role, settings[cls.DISPLAY_ROLE])
+            instance = cls._backend_instance[role]
+        return instance
+
+    @classmethod
+    def select_backend_from_role(cls, role, backend_name):
+        Cls = cls._get_backend_class(backend_name)
+        cls._backend_instance[role] = Cls()
+
+    @classmethod
+    def _get_backend_class(cls, backend_name):
+        modulename, classname = cls._backend_loading_info[backend_name]
+        try:
+            module = __import__(modulename, fromlist=classname)
+            BackendClass = module.__getattribute__(classname)
+        except ImportError, e:
+            raise ValueError("invalid backend : '%s' (%s)" % (backend_name, e))
+        return BackendClass
+
+
+class DisplayManagerMixin(object):
+
+    def initialize_display(self):
+        raise Exception("DisplayManagerMixin is an abstract class")
+
+    def create_axial_view(self, parent=None):
+        raise Exception("DisplayManagerMixin is an abstract class")
+
+    def set_bgcolor_views(self, views, rgba_color):
+        raise Exception("DisplayManagerMixin is an abstract class")
