brainvisa/morphologist

commit 644ebbfafa13a057a1435f7e06584fa53232b124

REFAC + STYLE

morphologist/gui/manage_study.py
@@ -62,9 +62,7 @@ class ManageStudyWindow(object):
     def __init__(self, study, parent=None):
         self.study = study
         uifile = os.path.join(ui_directory, 'manage_study.ui')
-        self.ui = loadUi(uifile)
-        if parent is not None:
-            self.ui.setParent(parent, QtCore.Qt.Window)
+        self.ui = loadUi(uifile, parent)
         apply_id = QtGui.QDialogButtonBox.Apply
         cancel_id = QtGui.QDialogButtonBox.Cancel
         self.ui.apply_button = self.ui.apply_cancel_buttons.button(apply_id)

morphologist/image_importation.py
@@ -1,9 +1,12 @@
-from .steps import Step
-from optparse import OptionParser
-from soma import aims
 import shutil
 import os
 import tempfile
+from optparse import OptionParser
+
+from soma import aims
+
+from .steps import Step
+
 
 class ImageImportation(Step):
     

morphologist/study.py
@@ -106,10 +106,7 @@ class Study(object):
 
     @staticmethod
     def define_subjectname_from_filename(filename):
-        name, ext = os.path.splitext(os.path.basename(filename))
-        while (ext != ""):
-            name, ext = os.path.splitext(name)
-        return name
+        return remove_all_extensions(filename)
 
     def add_subject_from_file(self, filename, subjectname=None, groupname=None):
         if subjectname is None:
@@ -173,6 +170,12 @@ class Study(object):
         s += 'outputdir :' + str(self.outputdir) + '\n'
         s += 'subjects :' + repr(self.subjects) + '\n'
         return s
+
+def remove_all_extensions(filename):
+    name, ext = os.path.splitext(os.path.basename(filename))
+    while (ext != ""):
+        name, ext = os.path.splitext(name)
+    return name
 
 
 class StudySerializationError(Exception):

morphologist/tests/gui/test_main_window.py
@@ -63,7 +63,7 @@ class TestStudyWidget(TestGui):
             self.assert_(subject.imgname.startswith(self.test_case.outputdir))
         
         main_window.ui.close()
-         
+
 
 class TestFlatFilesStudyWidget(TestStudyWidget):
 

morphologist/tests/gui/test_study.py
@@ -23,6 +23,7 @@ class TestStudyGui(TestGui):
         manage_subjects_window = ManageStudyWindow(study)
         self.keep_widget_alive(manage_subjects_window)
         manage_subjects_window.ui.show()
+        #FIXME: replace manage_subjects_window.ui by manage_subjects_window
         self.action_define_new_study_content(manage_subjects_window.ui,
             self.test_case.studyname, self.test_case.outputdir,
             self.test_case.filenames)

commit c15b0e198fb8efb52ca110ba6082f3097a42cc76

BUG: fix study creation dialog broken by 644ebbfafa13a057a1435 commit

morphologist/gui/manage_study.py
@@ -43,7 +43,7 @@ class SelectSubjectsDialog(QtGui.QFileDialog):
         self._study_manager.add_subjects(filenames)
 
 
-class ManageStudyWindow(object):
+class ManageStudyWindow(QtGui.QDialog):
     on_apply_cancel_button_clicked_map = {}
     default_group = 'group 1'
     group_column_width = 100
@@ -60,9 +60,14 @@ class ManageStudyWindow(object):
         map[reject_role] = cls.on_cancel_button_clicked
 
     def __init__(self, study, parent=None):
+        super(ManageStudyWindow, self).__init__(parent)
         self.study = study
         uifile = os.path.join(ui_directory, 'manage_study.ui')
-        self.ui = loadUi(uifile, parent)
+        self.ui = loadUi(uifile, self)
+        #self.ui = loadUi(uifile)
+        #if parent is not None:
+        #    print type(parent), type(self.ui)
+        #    self.ui.setParent(parent, QtCore.Qt.Window)
         apply_id = QtGui.QDialogButtonBox.Apply
         cancel_id = QtGui.QDialogButtonBox.Cancel
         self.ui.apply_button = self.ui.apply_cancel_buttons.button(apply_id)
@@ -95,7 +100,7 @@ class ManageStudyWindow(object):
         for filename in filenames:
             self._add_subject(filename, groupname)
 
-    @QtCore.Slot("QString &")
+    @QtCore.Slot("const QString &")
     def on_lineEdit_changed(self, text):
         outputdir = self.ui.outputdir_lineEdit.text()
         studyname = self.ui.studyname_lineEdit.text()