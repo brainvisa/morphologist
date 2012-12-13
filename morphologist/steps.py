fe5c49f2df7221fb9d0e9ceebf39cdc983e71ae5
Soizic LAGUITTON <sl231636@is147135.intra.cea.fr>
2012-12-13 17:51:12 +0100

    BUG: run method of Step

--- morphologist/steps.py
+++ morphologist/steps.py
@@ -8,8 +8,9 @@ class Step(object):
 
     def run(self):
         # WARNING do not overload: get_command should be used to run the step 
-        separator = " "
-        to_execute = separator.join(self.get_command())
+        to_execute = ""
+        for arg in self.get_command():
+            to_execute += "\"%s\" " % arg
         print "run the command: \n" + to_execute + "\n"
         return os.system(to_execute)
 
