82d83cdd7e1ba8f517bba1443f14d95ed641a8bd
Soizic LAGUITTON <sl231636@is147135.intra.cea.fr>
2012-12-14 17:28:57 +0100

    TEST: adds test_runner tests to tests/scripts/run_short.py

--- morphologist/tests/scripts/run_short.py
+++ morphologist/tests/scripts/run_short.py
@@ -6,6 +6,8 @@ from morphologist.tests.test_analysis import TestAnalysis
 from morphologist.tests.test_intra_analysis import TestIntraAnalysis
 from morphologist.tests.test_intra_analysis_study import TestBrainvisaTemplateStudy, TestDefaultTemplateStudy
 
+from morphologist.tests.test_runner import TestRunnerThread, TestRunnerSomaWorkflow
+
 if __name__=='__main__':
 
     suite = unittest.TestLoader().loadTestsFromTestCase(TestStudy)
@@ -15,6 +17,9 @@ if __name__=='__main__':
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestDefaultTemplateStudy))
 
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestIntraAnalysis))
+
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestRunnerThread))
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestRunnerSomaWorkflow))
 
     unittest.TextTestRunner(verbosity=2).run(suite)
 
