5a7d24ac82324839d48b59620fe661742766b9fe
Dominique Geffroy <dominique.geffroy@cea.fr>
2012-12-19 15:50:08 +0100

    REFAC: exception management

--- morphologist/image_importation.py
+++ morphologist/image_importation.py
@@ -23,20 +23,63 @@ class ImageImportation(Step):
                    self.output]
         return command
     
+    def run(self): 
+        temp_input = None
+        input_filename = self.input
+        return_value = 0
+        
+        try:
+            input_vol = aims.read(self.input)
+        except (aims.aimssip.IOError, IOError) as e:
+            raise ImportationError(e.message)
+
+        if self._conversion_needed(input_filename, input_vol):
+            try:
+                if self._remove_nan_needed(input_vol):
+                    (temp_file, temp_input) = tempfile.mkstemp()
+                    temp_file.close()
+                    command = "AimsRemoveNaN -i '%s' -o '%s'" % (self.input, temp_input)
+                    return_value = os.system(command)
+                    if return_value != 0:
+                        raise ImportationError("The following command failed : %s" % command)
+                    input_filename = temp_input
+
+                command_list = ['AimsFileConvert', '-i', input_filename, '-o', self.output, 
+                           '-t', 'S16']
+                if self._resampling_needed(input_vol):
+                    command_list.extend([ '-r', '--omin', 0, '--omax', 4095 ])
+                command = " ".join(command_list)
+                return_value = os.system(command)
+                if return_value != 0:
+                    raise ImportationError("The following command failed : %s" % command)
+            finally:
+                if temp_input is not None:
+                    os.remove(temp_input)                
+
+        else:
+            try:
+                shutil.copy(self.input, self.output)
+                if os.path.exists(self.input+".minf"):
+                    shutil.copy(self.input+".minf", self.output+".minf")
+            except IOError as e:
+                raise ImportationError(e.message)
+        
+        return return_value
+
     @staticmethod
-    def conversion_needed(input_filename, input_vol):
+    def _conversion_needed(input_filename, input_vol):
         convert = False
         if not input_filename.endswith(".nii"):
             convert = True
         else:
             header = input_vol.header()
-            data_type=header["data_type"]
+            data_type = header["data_type"]
             file_format = header["file_type"] 
             convert = (file_format != "NIFTI1") or (data_type != "S16")
         return convert
       
     @staticmethod  
-    def resampling_needed(input_vol):
+    def _resampling_needed(input_vol):
         header = input_vol.header()
         data_type=header["data_type"]
         need_resampling = False
@@ -50,50 +93,11 @@ class ImageImportation(Step):
         return need_resampling
     
     @staticmethod
-    def remove_nan_needed(input_vol):
+    def _remove_nan_needed(input_vol):
         header = input_vol.header()
         data_type=header["data_type"]
         return data_type in ('FLOAT', 'DOUBLE')
-            
-    def run(self):
-        print "Run image importation step on ", self.input, self.output    
-        temp_input = None
-        input_filename = self.input
-        return_value = 0
-        
-        try:
-            input_vol = aims.read(self.input)
-            if self.conversion_needed(input_filename, input_vol):
-                if self.remove_nan_needed(input_vol):
-                    (temp_file, temp_input) = tempfile.mkstemp()
-                    temp_file.close()
-                    command = "AimsRemoveNaN -i '%s' -o '%s'" % (self.input, temp_input)
-                    return_value = os.system(command)
-                    if return_value != 0:
-                        raise ImportationError("The following command failed : %s" % command)
-                    input_filename = temp_input
-
-                command_list = ['AimsFileConvert', '-i', input_filename, '-o', self.output, 
-                           '-t', 'S16']
-                if self.resampling_needed(input_vol):
-                    command_list.extend([ '-r', '--omin', 0, '--omax', 4095 ])
-                command = " ".join(command_list)
-                return_value = os.system(command)
-                if return_value != 0:
-                    raise ImportationError("The following command failed : %s" % command)
-
-            else:
-                shutil.copy(self.input, self.output)
-                if os.path.exists(self.input+".minf"):
-                    shutil.copy(self.input+".minf", self.output+".minf")
-        except Exception, e:
-            raise ImportationError(e.message)
-        finally:
-            if temp_input is not None:
-                os.remove(temp_input)
-        
-        return return_value
-     
+             
      
 class ImportationError(Exception):
     pass   
