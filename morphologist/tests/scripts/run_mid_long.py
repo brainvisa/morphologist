6a57583c803d4c2c6aa166f7066beb154cee7090
Soizic LAGUITTON <sl231636@is147135.intra.cea.fr>
2013-01-09 10:28:14 +0100

    TEST: adds test_object3d to run_mid_long script

--- morphologist/tests/scripts/run_mid_long.py
+++ morphologist/tests/scripts/run_mid_long.py
@@ -1,10 +1,12 @@
 import unittest
 
 from morphologist.tests.intra_analysis.test_runner_mock_analysis import TestRunnerMockIntraAnalysisSomaWorkflow
+from morphologist.tests.test_object3d import TestObject3D
 
 if __name__=='__main__':
 
     suite = unittest.TestLoader().loadTestsFromTestCase(TestRunnerMockIntraAnalysisSomaWorkflow)
+    suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestObject3D))
 
     unittest.TextTestRunner(verbosity=2).run(suite)
 
