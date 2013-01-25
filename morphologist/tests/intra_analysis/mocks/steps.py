9291ef760d0bd57600df230f29cba454326666e0
Matthieu Perrot <matthieu.perrot@gmail.com>
2013-01-25 17:04:54 +0100

    ENH: automatize writting of mock intra analysis steps

--- morphologist/tests/intra_analysis/mocks/steps.py
+++ morphologist/tests/intra_analysis/mocks/steps.py
@@ -6,153 +6,133 @@ from morphologist.intra_analysis_steps import SpatialNormalization, \
 from morphologist.intra_analysis import IntraAnalysis
 
 
-class MockSpatialNormalization(SpatialNormalization):
+class AutomockStep(object):
+
+    def _get_inputs(self):
+        file_inputs = self._get_outputs()
+        other_inputs = []
+        return file_inputs, other_inputs
+
+    def get_command(self):
+        command = ['python', '-m',
+                   'morphologist.tests.intra_analysis.mocks.steps',
+                   self.name]
+        parameters = []
+        for parameter in self.outputs.list_file_parameter_names():
+            parameters.append(self.inputs[parameter])
+            parameters.append(self.outputs[parameter])
+        command.extend(parameters)
+        return command
+
+    def step_name(self):
+        raise NotImplementedError("AutomockStep is an abstract class")
+
+
+class MockSpatialNormalization(AutomockStep, SpatialNormalization):
     
     def __init__(self, mock_out_files):
         super(MockSpatialNormalization, self).__init__()
         self.out_files = mock_out_files
-        
-    def get_command(self):
-        command = ['python', '-m',
-            'morphologist.tests.intra_analysis.mocks.steps',
-            'normalization',
-            self.out_files[IntraAnalysis.COMMISSURE_COORDINATES], self.commissure_coordinates,
-            self.out_files[IntraAnalysis.TALAIRACH_TRANSFORMATION], self.talairach_transformation]
-        return command
+        self.name = 'normalization'
+        self.inputs.commissure_coordinates = self.out_files[IntraAnalysis.COMMISSURE_COORDINATES]
+        self.inputs.talairach_transformation = self.out_files[IntraAnalysis.TALAIRACH_TRANSFORMATION]
 
 
-class MockBiasCorrection(BiasCorrection):
+class MockBiasCorrection(AutomockStep, BiasCorrection):
 
     def __init__(self, mock_out_files):
         super(MockBiasCorrection, self).__init__()
         self.out_files = mock_out_files
+        self.name = 'bias_correction'
+        self.inputs.hfiltered = self.out_files[IntraAnalysis.HFILTERED]
+        self.inputs.white_ridges = self.out_files[IntraAnalysis.WHITE_RIDGES]
+        self.inputs.edges = self.out_files[IntraAnalysis.EDGES]
+        self.inputs.corrected_mri = self.out_files[IntraAnalysis.CORRECTED_MRI]
+        self.inputs.variance = self.out_files[IntraAnalysis.VARIANCE]
  
-    def get_command(self):
-        command = ['python', '-m',
-            'morphologist.tests.intra_analysis.mocks.steps', 'bias_correction',
-            self.out_files[IntraAnalysis.HFILTERED], self.hfiltered,
-            self.out_files[IntraAnalysis.WHITE_RIDGES], self.white_ridges, 
-            self.out_files[IntraAnalysis.EDGES], self.edges, 
-            self.out_files[IntraAnalysis.CORRECTED_MRI], self.corrected_mri, 
-            self.out_files[IntraAnalysis.VARIANCE], self.variance]
-        return command
 
-
-class MockHistogramAnalysis(HistogramAnalysis):
+class MockHistogramAnalysis(AutomockStep, HistogramAnalysis):
     
     def __init__(self, mock_out_files):
         super(MockHistogramAnalysis, self).__init__()
         self.out_files = mock_out_files
- 
-    def get_command(self):
-        command = ['python', '-m',
-            'morphologist.tests.intra_analysis.mocks.steps',
-            'histogram_analysis',
-            self.out_files[IntraAnalysis.HISTO_ANALYSIS], self.histo_analysis,
-            self.out_files[IntraAnalysis.HISTOGRAM], self.histogram]
-        return command
+        self.name = 'histogram_analysis'
+        self.inputs.histo_analysis = self.out_files[IntraAnalysis.HISTO_ANALYSIS]
+        self.inputs.histogram = self.out_files[IntraAnalysis.HISTOGRAM]
 
 
-class MockBrainSegmentation(BrainSegmentation):
+class MockBrainSegmentation(AutomockStep, BrainSegmentation):
     
     def __init__(self, mock_out_files):
         super(MockBrainSegmentation, self).__init__()
         self.out_files = mock_out_files
+        self.name = 'brain_segmentation'
+        self.inputs.brain_mask = self.out_files[IntraAnalysis.BRAIN_MASK]
+        self.inputs.white_ridges = self.out_files[IntraAnalysis.WHITE_RIDGES]
  
-    def get_command(self):
-        command = ['python', '-m',
-            'morphologist.tests.intra_analysis.mocks.steps',
-            'brain_segmentation',
-            self.out_files[IntraAnalysis.BRAIN_MASK], self.brain_mask,
-            self.out_files[IntraAnalysis.WHITE_RIDGES], self.white_ridges]
-        return command
 
-
-class MockSplitBrain(SplitBrain):
+class MockSplitBrain(AutomockStep, SplitBrain):
     
     def __init__(self, mock_out_files):
         super(MockSplitBrain, self).__init__()
         self.out_files = mock_out_files
+        self.name = 'split_brain'
+        self.inputs.split_mask = self.out_files[IntraAnalysis.SPLIT_MASK]
  
-    def get_command(self):
-        command = ['python', '-m',
-            'morphologist.tests.intra_analysis.mocks.steps', 'split_brain',
-            self.out_files[IntraAnalysis.SPLIT_MASK], self.split_mask]
-        return command
 
-
-class MockGreyWhite(GreyWhite):
+class MockGreyWhite(AutomockStep, GreyWhite):
     
     def __init__(self, ref_grey_white):
         super(MockGreyWhite, self).__init__()
-        self.ref_grey_white = ref_grey_white
+        self.name = 'grey_white'
+        self.inputs.grey_white = ref_grey_white
  
-    def get_command(self):
-        command = ['python', '-m',
-            'morphologist.tests.intra_analysis.mocks.steps',
-            'grey_white',
-            self.ref_grey_white, 
-            self.grey_white]
-        return command
 
-
-class MockGrey(Grey):
+class MockGrey(AutomockStep, Grey):
     
     def __init__(self, ref_grey):
         super(MockGrey, self).__init__()
-        self.ref_grey = ref_grey
+        self.name = 'grey'
+        self.inputs.grey = ref_grey
         
-    def get_command(self):
-        command = ['python', '-m', 
-                   'morphologist.tests.intra_analysis.mocks.steps',
-                   'grey',
-                   self.ref_grey,
-                   self.grey]
-        return command
-
     
-class MockWhiteSurface(WhiteSurface):
+class MockWhiteSurface(AutomockStep, WhiteSurface):
     
     def __init__(self, ref_white_surface):
         super(MockWhiteSurface, self).__init__()
-        self.ref_white_surface = ref_white_surface
+        self.name = 'white_surface'
+        self.inputs.white_surface = ref_white_surface
         
-    def get_command(self):
-        command = ['python', '-m', 
-                   'morphologist.tests.intra_analysis.mocks.steps',
-                   'white_surface',
-                   self.ref_white_surface,
-                   self.white_surface]
-        return command
 
 class MockGreySurface(GreySurface):
   
     def __init__(self, ref_grey_surface):
         super(MockGreySurface, self).__init__()
-        self.ref_grey_surface = ref_grey_surface
+        self.name = 'grey_surface'
+        self.inputs.grey_surface = ref_grey_surface
 
     def get_command(self):
         command = ['python', '-m',
-                   'morphologist.tests.intra_analysis.mocks.steps',
-                   'grey_surface',
-                   self.ref_grey_surface,
-                   self.grey_surface]
+                   'morphologist.tests.intra_analysis.mocks.steps', self.name,
+                   self.inputs.grey_surface,
+                   self.outputs.grey_surface]
         return command
+
 
 class MockSulci(Sulci):
   
     def __init__(self, ref_sulci):
         super(MockSulci, self).__init__()
-        self.ref_sulci = ref_sulci
+        self.name = 'sulci'
+        self.inputs.sulci = ref_sulci
 
     def get_command(self):
         command = ['python', '-m',
-                   'morphologist.tests.intra_analysis.mocks.steps',
-                   'sulci',
-                   self.ref_sulci,
-                   self.sulci,
-                   self.ref_sulci.replace(".arg", ".data"),
-                   self.sulci.replace(".arg", ".data")]
+                   'morphologist.tests.intra_analysis.mocks.steps', self.name,
+                   self.inputs.sulci,
+                   self.outputs.sulci,
+                   self.inputs.sulci.replace(".arg", ".data"),
+                   self.outputs.sulci.replace(".arg", ".data")]
         return command
 
 
@@ -160,20 +140,19 @@ class MockSulciLabelling(SulciLabelling):
 
     def __init__(self, ref_labeled_sulci):
         super(MockSulciLabelling, self).__init__()
-        self.ref_labeled_sulci = ref_labeled_sulci
+        self.name = 'sulci_labelling'
+        self.inputs.labeled_sulci = ref_labeled_sulci
 
     def get_command(self):
         command = ['python', '-m',
-                   'morphologist.tests.intra_analysis.mocks.steps',
-                   'sulci_labelling',
-                   self.ref_labeled_sulci,
-                   self.labeled_sulci,
-                   self.ref_labeled_sulci.replace(".arg", ".data"),
-                   self.labeled_sulci.replace(".arg", ".data")]
+                   'morphologist.tests.intra_analysis.mocks.steps', self.name,
+                   self.inputs.labeled_sulci,
+                   self.outputs.labeled_sulci,
+                   self.inputs.labeled_sulci.replace(".arg", ".data"),
+                   self.outputs.labeled_sulci.replace(".arg", ".data")]
         return command
 
 
-  
 def main():
     import time, sys, shutil
  
