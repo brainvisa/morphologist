c0cf680ed378a4fb79701d15e8c0171a0044ab6b
Matthieu Perrot <matthieu.perrot@gmail.com>
2013-01-29 18:23:03 +0100

    STYLE: rename TestRunnerSomaWorkflow -> TestSomaWorkflowRunner and others

--- morphologist/tests/scripts/run_mid_long.py
+++ morphologist/tests/scripts/run_mid_long.py
@@ -1,11 +1,11 @@
 import unittest
 
-from morphologist.tests.intra_analysis.test_runner_mock_analysis import TestRunnerMockIntraAnalysisSomaWorkflow
+from morphologist.tests.intra_analysis.test_runner_mock_analysis import TestMockIntraAnalysisSomaWorkflowRunner
 from morphologist.tests.test_object3d import TestObject3D
 
 if __name__=='__main__':
 
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestRunnerMockIntraAnalysisSomaWorkflow)
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMockIntraAnalysisSomaWorkflowRunner)
     suite.addTest(unittest.TestLoader().loadTestsFromTestCase(TestObject3D))
 
     unittest.TextTestRunner(verbosity=2).run(suite)
