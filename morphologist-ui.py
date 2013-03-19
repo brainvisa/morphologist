3c7dde5ce690c548a2e4c24d62ac57c03282b776
Matthieu Perrot <matthieu.perrot@gmail.com>
2013-03-19 10:11:13 +0100

    ENH: priorities: specified commandline opt < settings < default commandline opt
    settings from config files have superior priority than default commandline
    options, but less than specified commandline options

--- morphologist-ui.py
+++ morphologist-ui.py
@@ -15,24 +15,31 @@ def option_parser():
     
     group_neurospin = optparse.OptionGroup(parser, "Neurospin specific options")
     parser.add_option_group(group_neurospin)
-    group_neurospin.add_option('--brainomics', action='store_true', 
+    group_neurospin.add_option('--brainomics', action='callback', 
+                               callback=brainomics_option_callback,
                                dest='brainomics', default=False,
                                help="Images can be imported in a study from "
                                "Brainomics Cubicweb database")
     group_debug = optparse.OptionGroup(parser, "Debug options")
     parser.add_option_group(group_debug)
-    group_debug.add_option('--mock', action="store_true", 
-                      dest='mock', default=False,
-                      help="Test mode, runs mock intra analysis") 
+    group_debug.add_option('--mock', action="callback",
+            callback=mock_option_callback, dest='mock', default=False,
+            help="Test mode, runs mock intra analysis") 
    
     return parser
+
+
+def mock_option_callback(self, option, value, parser):
+    settings['debug']['mock'] = True
+
+
+def brainomics_option_callback(self, option, value, parser):
+    settings['application']['brainomics'] = True
+
 
 def main():
     parser = option_parser()
     options, args = parser.parse_args(sys.argv)
-
-    settings['application']['brainomics'] = options.brainomics
-    settings['debug']['mock'] = options.mock
     if not settings['settings_validation']: return
     
     qApp = QtGui.QApplication(sys.argv)
