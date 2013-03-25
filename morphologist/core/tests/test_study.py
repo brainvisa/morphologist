4c3aaa983a02279d9cb7ebe734de6fe82217652d
Dominique Geffroy <dominique.geffroy@cea.fr>
2013-03-25 15:26:45 +0100

    TEST: More tests in test_study
    Tests for methods has_subjects, has_some_results, remove_subject_from_id

--- morphologist/core/tests/test_study.py
+++ morphologist/core/tests/test_study.py
@@ -3,6 +3,7 @@ import os
 import filecmp
 
 from morphologist.core.study import SubjectExistsError
+from morphologist.core.subject import Subject
 from morphologist.core.tests.study import MockStudyTestCase
 
 
@@ -11,32 +12,52 @@ class TestStudy(unittest.TestCase):
     def setUp(self):
         self.test_case = self.create_test_case()
         self.test_case.create_study()
-        self.test_case.add_subjects()
         self.study = self.test_case.study
+        self.subject = Subject(self.test_case.subjectnames[0], 
+                               self.test_case.groupnames[0], 
+                               self.test_case.filenames[0]) 
  
     def test_subject_exists_error(self):
-        first_subject_id = next(self.study.subjects.iterkeys())
-        existing_subject = self.study.subjects[first_subject_id]
+        self.study.add_subject(self.subject)
         
         self.assertRaises(SubjectExistsError, 
-                          self.test_case.study_cls().add_subject,
-                          self.study,
-                          existing_subject) 
+                          self.study.add_subject,
+                          self.subject) 
    
     def test_save_load_study(self):
+        self.test_case.add_subjects()
+        
         studyfilepath = self.study.backup_filename
         studyfilepath2 = studyfilepath + "_2"
         if os.path.isfile(studyfilepath): os.remove(studyfilepath)
         if os.path.isfile(studyfilepath2): os.remove(studyfilepath2)
-        print "save to " + repr(studyfilepath)
-
         self.study.save_to_backup_file()
+        
         loaded_study = self.test_case.study_cls().from_file(studyfilepath)
         loaded_study.backup_filename = studyfilepath2
         loaded_study.save_to_backup_file()
 
         self.assert_(filecmp.cmp(studyfilepath, studyfilepath2))
 
+    def test_has_subjects(self):
+        self.assert_(not self.study.has_subjects())
+        
+        self.study.add_subject(self.subject)
+        
+        self.assert_(self.study.has_subjects())
+        
+    def test_has_some_results(self):
+        self.test_case.add_subjects()
+        self.test_case.create_some_output_files()
+        
+        self.assert_(self.study.has_some_results())
+        
+    def test_remove_subject(self):
+        self.study.add_subject(self.subject)
+        self.study.remove_subject_from_id(self.subject.id())
+        
+        self.assert_(not self.study.has_subjects())
+        
     def create_test_case(self):
         test_case = MockStudyTestCase()
         return test_case
