2eb2b4d4a62064e66e7774c454c9dfe6b2bec950
Denis Riviere <denis.riviere@cea.fr>
2014-06-11 10:27:10 +0200

    don't do anything in the general module morphologist init code

--- morphologist/__init__.py
+++ morphologist/__init__.py
@@ -1,3 +1,3 @@
 # XXX It is necessary to import intra_analysis to register IntraAnalysis in the AnalysisFactory
-import intra_analysis
-import tests.intra_analysis.mocks.analysis
+# import intra_analysis
+# import tests.intra_analysis.mocks.analysis
