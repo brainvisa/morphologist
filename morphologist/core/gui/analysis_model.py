05769d4ef7c29fbe24be612ed559eaca11b1229d
Denis Riviere <denis.riviere.gm@gmail.com>
2015-09-01 18:26:11 +0200

    ENH: setting things up for threaded polling

--- morphologist/core/gui/analysis_model.py
+++ morphologist/core/gui/analysis_model.py
@@ -7,30 +7,93 @@ from morphologist.core.gui.qt_backend import QtCore
 
 class AnalysisPollingThread(QtCore.QThread):
 
+    files_changed = QtCore.pyqtSignal(dict)
     STOPPED = 0
     RUNNING = 1
     HELD = 2
 
-    def __init__(self, observed_files):
+    def __init__(self, analysis):
         self.lock = threading.RLock()
         self._update_interval = 4 # in seconds
         self.state = self.STOPPED
-        self.observed_files = observed_files
+        self.set_analysis(analysis)
+
+    def set_analysis(self, analysis):
+        with self.lock:
+            self._analysis = analysis
+            self._output_parameters_file_sha = {}
+            self.observed_files = self._get_observed_files()
+            #self.changed.emit()
+            #self._emit_input_files_changed()
 
     def run(self):
         with self.lock:
             self.state = self.RUNNING
         while True:
-            with self.lock:
                 time.sleep(self._update_interval * 1000)
-                state = int(self.state)
-            if state == self.STOPPED:
-                break
-            elif state == RUNNING:
-                self._check_output_files_changed()
+                with self.lock:
+                    state = int(self.state)
+                if state == self.STOPPED:
+                    break
+                elif state == RUNNING:
+                    self._check_output_files_changed()
+
+    def _get_observed_files(self):
+        checked_outputs_names \
+            = self._analysis.get_output_file_parameter_names()
+        self._analysis.propagate_parameters()
+        checked_outputs = dict([
+            (parameter_name,
+             getattr(self._analysis.pipeline.process, parameter_name))
+            for parameter_name in checked_outputs_names])
+        #checked_outputs = \
+            #self._analysis.list_output_parameters_with_existing_files()
+        #diffs = [k for k, v in checked_outputs1.iteritems() if k not in checked_outputs or checked_outputs[k] != v]
+        #if diffs:
+            #print '** DIFFS in _check_output_files_changed:', dict([(k, (checked_outputs1[k], checked_outputs.get(k))) for k in diffs])
+        return checked_outputs
 
     def _check_output_files_changed(self):
-        pass
+        with self.lock:
+            checked_outputs = dict(self.observed_files)
+        changed_parameters = self._changed_parameters(
+            checked_outputs, self._output_parameters_file_sha)
+        if len(changed_parameters) > 0:
+            changed_parameters_with_details = {}
+            for parameter_name in changed_parameters:
+                changed_parameters_with_details[parameter_name] = \
+                        getattr(checked_outputs, parameter_name)
+            self.files_changed.emit(changed_parameters_with_details)
+
+    def _changed_parameters(self, existing_items, parameters_file_sha):
+        changed_parameters = []
+        for parameter_name, filename in existing_items.iteritems():
+            if filename is None or not os.path.exists(filename):
+                # missing files are considered not changed
+                # (which may be a problem if files are removed)
+                continue
+            # TODO: directories are ignored !
+            if os.path.isdir(filename): continue
+            last_sha = parameters_file_sha.get(parameter_name, None)
+            new_sha = self._sha(filename)
+            if new_sha != last_sha:
+                parameters_file_sha[parameter_name] = new_sha
+                changed_parameters.append(parameter_name)
+
+        prev_parameters = set(parameters_file_sha.keys())
+        new_parameters = set(existing_items.keys())
+        deleted_parameters = prev_parameters.difference(new_parameters)
+        for parameter_name in deleted_parameters:
+            changed_parameters.append(parameter_name)
+            del parameters_file_sha[parameter_name]
+
+        return changed_parameters
+
+    def _sha(self, filename):
+        with open(filename, "r") as fd:
+            content = fd.read()
+            sha = hashlib.sha1(content)
+        return sha.hexdigest()
 
 
 class LazyAnalysisModel(QtCore.QObject):
@@ -67,15 +130,23 @@ class LazyAnalysisModel(QtCore.QObject):
         self.files_changed.emit(
             self._analysis.list_input_parameters_with_existing_files())
 
+    def _get_observed_files(self):
+        checked_outputs_names \
+            = self._analysis.get_output_file_parameter_names()
+        self._analysis.propagate_parameters()
+        checked_outputs = dict([
+            (parameter_name,
+             getattr(self._analysis.pipeline.process, parameter_name))
+            for parameter_name in checked_outputs_names])
+        #checked_outputs = \
+            #self._analysis.list_output_parameters_with_existing_files()
+        #diffs = [k for k, v in checked_outputs1.iteritems() if k not in checked_outputs or checked_outputs[k] != v]
+        #if diffs:
+            #print '** DIFFS in _check_output_files_changed:', dict([(k, (checked_outputs1[k], checked_outputs.get(k))) for k in diffs])
+        return checked_outputs
+
     def _check_output_files_changed(self):
-        #checked_outputs_names \
-            #= self._analysis.get_output_file_parameter_names()
-        #checked_outputs = dict([
-            #(parameter_name,
-             #getattr(self._analysis.pipeline.process, parameter_name))
-            #for parameter_name in checked_outputs_names])
-        checked_outputs = \
-            self._analysis.list_output_parameters_with_existing_files()
+        checked_outputs = self._get_observed_files()
         changed_parameters = self._changed_parameters(
             checked_outputs, self._output_parameters_file_sha)
         if len(changed_parameters) > 0:
@@ -89,7 +160,7 @@ class LazyAnalysisModel(QtCore.QObject):
     def _changed_parameters(self, existing_items, parameters_file_sha):
         changed_parameters = []
         for parameter_name, filename in existing_items.iteritems():
-            if not os.path.exists(filename):
+            if filename is None or not os.path.exists(filename):
                 # missing files are considered not changed
                 # (which may be a problem if files are removed)
                 continue
