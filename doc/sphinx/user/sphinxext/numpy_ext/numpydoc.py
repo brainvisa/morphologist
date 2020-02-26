3730bb861d90fc9558149674070e8d4b4f3ee5f4
Yann Cointepas <yann@cointepas.net>
2020-02-26 16:21:04 +0100

    Update numpydoc.py

--- doc/sphinx/user/sphinxext/numpy_ext/numpydoc.py
+++ doc/sphinx/user/sphinxext/numpy_ext/numpydoc.py
@@ -46,10 +46,7 @@ def mangle_docstrings(app, what, name, obj, options, lines,
         lines[:] = title_re.sub(sixu(''), sixu("\n").join(lines)).split(sixu("\n"))
     else:
         doc = get_doc_object(obj, what, sixu("\n").join(lines), config=cfg)
-        if sys.version_info[0] >= 3:
-            doc = str(doc)
-        else:
-            doc = six.text_type(doc)
+        doc = six.text_type(doc)
         lines[:] = doc.split(sixu("\n"))
 
     if app.config.numpydoc_edit_link and hasattr(obj, '__name__') and \
