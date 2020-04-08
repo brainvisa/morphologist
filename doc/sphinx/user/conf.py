57805bac4893a5be730ed4f0eb1e86c5edc3e423
Denis Rivière <denis.riviere.gm@gmail.com>
2020-04-08 09:59:33 +0200

    fixes in doc config

--- doc/sphinx/user/conf.py
+++ doc/sphinx/user/conf.py
@@ -310,13 +310,16 @@ docpath = os.path.join( os.path.dirname( os.path.dirname( os.path.dirname( \
   brainvisa.__file__ ) ) ), 'share', 'doc' )
 
 intersphinx_mapping = {
-  'somabase': ( os.path.join( docpath, 'soma-base-' + version + '/sphinx' ),
-               None ),
-  'pyaims': ( os.path.join( docpath, 'pyaims-' + version + '/sphinx' ), None ),
-  'pyana': ( os.path.join( docpath, 'pyanatomist-' + version + '/sphinx' ),
+  'somabase': (os.path.join(docpath, 'soma-base-' + somabase_version
+                            + '/sphinx'),
+               None),
+  'pyaims': (os.path.join(docpath, 'pyaims-' + aims_version
+                          + '/sphinx'), None),
+  'pyana': (os.path.join(docpath, 'pyanatomist-' + anatmist_version
+                         + '/sphinx'),
             None ),
   'somaworkflow': ( os.path.join( docpath, 'soma-workflow-' \
     + somaworkflow_version + '/sphinx' ), None ),
-  'python': ('http://docs.python.org/2.7', None),
+  'python': ('http://docs.python.org/%d.%d' % sys.version_info[:2], None),
 }
 
